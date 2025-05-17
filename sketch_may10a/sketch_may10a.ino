b465d5d0a1e931353aaeff5b8560167161088abd
Benja hola <escribanagustin8@gmail.com>
2025-05-17 12:07:45 -0300

    Buzzer sound

--- sketch_may10a/sketch_may10a.ino
+++ sketch_may10a/sketch_may10a.ino
@@ -1,26 +1,31 @@
 int green = 9;
 int red = 8;
 int blue = 10;
+int buzzer = 11;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
+  pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  luces()
+  luces();
 }
 void luces() {
   digitalWrite(red, HIGH);
   digitalWrite(red, LOW);
-  delay(300)
+  analogWrite(buzzer, 720);
+  delay(300);
   digitalWrite(green,HIGH);
   digitalWrite(green, LOW);
+  analogWrite(buzzer, 520);
   delay(300);
   digitalWrite(blue, HIGH);
   digitalWrite(blue, LOW);
+  analogWrite(buzzer, 827);
   delay(300); 
 }
